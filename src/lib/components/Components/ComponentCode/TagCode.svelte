8f315d9213a57509dc7c66424e87a48c107063bc
dev14 <dev14@webteamwork.ca>
2024-04-15 11:28:10 +0330

    add button component

---
+++ src/lib/components/Components/ComponentCode/TagCode.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let name = '';
+	export let props: any = [];
+
+	function propAttr(prop: any) {
+		const string = prop.split(':');
+		console.log(string, 'biya ino bebin');
+		return string;
+	}
+</script>
+
+<span>
+	&lt;<span class="text-green-600">{name}</span>
+	{#each props as prop, index}
+		{@const propValues = propAttr(prop)}
+		<span>
+			{propValues[0]}{#if propValues[1]}="{propValues[1]}"{/if}
+		</span>
+		{#if index < props.length - 1}
+			<span></span>
+		{/if}
+	{/each}
+	<span>&gt;</span>
+	<slot />
+	&lt;/<span class="text-green-600">{name}</span>&gt;
+</span>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { purgeCss } from 'vite-plugin-svelte-purgecss';
+import examples from 'mdsvexamples/vite';
+import path from 'path';
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), examples],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	resolve: {
+		alias: {
+		  'uikit': path.resolve(process.cwd(), './src/lib/index.ts')
+		}
+	  }
 });
