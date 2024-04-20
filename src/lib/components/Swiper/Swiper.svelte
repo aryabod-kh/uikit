365a17c4576e98c52d6688f8b935c97b5621a12b
Armin <arminkheirkhahan@gmail.com>
2024-04-20 21:30:37 +0330

    add swiper

---
+++ src/lib/components/Swiper/Swiper.svelte
@@ -0,0 +1,51 @@
+<script>
+	import { register } from 'swiper/element/bundle';
+	// register Swiper custom elements
+	register();
+	export let slidesPerView = '6';
+	export let spaceBetween = '10';
+	export let centered = false;
+	export let breakpoints = {
+		320: {
+			slidesPerView: Math.round(+slidesPerView / 5)
+		},
+		640: {
+			slidesPerView: Math.round(+slidesPerView / 3)
+		},
+		768: {
+			slidesPerView: Math.round(+slidesPerView / 2)
+		},
+		1024: {
+			slidesPerView: slidesPerView
+		}
+	};
+	export let navigation = false;
+	export let pagination = false;
+	export let lazy = false;
+	const onProgress = (e) => {
+		const [swiper, progress] = e.detail;
+		console.log(progress);
+	};
+	const onSlideChange = (e) => {
+		console.log('slide changed');
+	};
+</script>
+
+<!-- {pagination}
+{navigation}
+{breakpoints}
+{lazy} -->
+
+<swiper-container
+	slides-per-view={slidesPerView}
+	space-between={spaceBetween}
+	centered-slides={centered}
+	{pagination}
+	{navigation}
+	{lazy}
+	{breakpoints}
+	on:swiperprogress
+	on:swiperslidechange
+>
+	<slot />
+</swiper-container>
